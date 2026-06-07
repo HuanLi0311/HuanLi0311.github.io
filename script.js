08184fac9c8be4768c992afa52b263cb26d3db9f
huanli <huanhuanli104@gmail.com>
2026-06-07 14:03:46 +0800

    Use academic homepage layout

--- script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector(".site-header");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 8) {
-    header?.classList.add("is-scrolled");
-  } else {
-    header?.classList.remove("is-scrolled");
-  }
-});
